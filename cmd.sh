b779da7bb0e77a75245631b87ddc62627e9ed804
jgas <joaoguilhermeas453@gmail.com>
2026-03-01 14:16:03 -0300

    a

--- cmd.sh
+++ cmd.sh
@@ -9,11 +9,21 @@ fi
 sudo swapoff -a #disable current swap
 
 cmd=$(sudo fallocate -l ${DEFAULT_MEMORY}G /swapfile)
+echo "Creating swap file with command: $cmd"
+
 sudo chmod 600 /swapfile #only readable/writeable by root
+echo "Setting permissions for /swapfile to 600"
+
 sudo mkswap /swapfile #make the file a swap file
+echo "Making /swapfile a swap file"
+
 sudo swapon /swapfile #enable the swap file
+echo "Enabling /swapfile as swap"
 
+echo "Swap file created and enabled with size ${DEFAULT_MEMORY}G"
 
+echo "To maintain swap after reboot, add the following line to /etc/fstab:"
+echo "/swapfile none swap sw 0 0"
 # How to maintain swap after reboot:
 # Add the following line to /etc/fstab:
 # /swapfile none swap sw 0 0
